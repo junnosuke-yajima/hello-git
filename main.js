6dcbf09a53582ec322f3ed8e1ff31b7fc7dad62d
junnosuke.yajima <becreative.jun@gmail.com>
2021-02-18 14:36:40 +0900

    async OK

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 $(function(){
-    $.ajaxSetup({cache:false});
     $("header").load("./include/header.html");
-});
-$(function(){
-    $.ajaxSetup({cache:false});
     $("footer").load("./include/footer.html");
 });
