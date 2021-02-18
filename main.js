708bdc422bd89ffd02d31607bf0659a9a7801f9a
junnosuke.yajima <becreative.jun@gmail.com>
2021-02-18 14:21:52 +0900

    hamberger fix

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ $(function(){
     $.ajaxSetup({cache:false});
     $("header").load("./include/header.html");
 });
+$(function(){
+    $.ajaxSetup({cache:false});
+    $("footer").load("./include/footer.html");
+});
